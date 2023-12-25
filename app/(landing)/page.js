ed1664ff980aa612bf9eeec493d563cd078f111a
kgaurav152 <kgaurav152@gmail.com>
2023-12-25 14:46:35 +0530

    modified register now button to add animation

--- app/(landing)/page.js
+++ app/(landing)/page.js
@@ -3,7 +3,7 @@
 import { Button } from "@/components/ui/button"
 import { ArrowRight, MoveUpRight } from "lucide-react";
 import { useRouter } from "next/navigation";
-import Link from "next/link";
+// import Link from "next/link";
 import NavBar from '@/components/navbar';
 import Footer from '@/components/footer';
 import { useState, useEffect } from "react";
@@ -53,23 +53,22 @@ const LandingPage = () => {
             <div className="flex flex-col items-center">
               <h1 className="font-bold text-[3rem]">TechFest 2024</h1>
               <p className="text-[1.5rem] mb-5">Tentative Date: 22-26 January 2024</p>
-              <button
-                className="relative rounded-lg mb-2 pt-2 pb-2 pr-4 pl-4 border border-transparent bg-gray-900 text-white px-5 py-2 transition-all duration-300 hover:bg-purple-500 hover:text-white flex items-center"
+              <Button
+                className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 relative rounded-2xl mb-2 pt-2 pb-2 pr-4 pl-4 border border-transparent bg-gray-900 text-white px-5 py-2 hover:bg-purple-500 hover:text-white flex items-center hover:border-white"
                 onMouseEnter={()=>{setIsHovered(true);}}
                 onMouseLeave={()=>{setIsHovered(false);}} 
                 onClick={(e) => handleClick(e, "/registration")}
               >
                 <div className="flex items-center">
-                  <div
-                    className={`w-7 h-7 rounded-full flex items-center justify-center hover:bg-purple-500 border-white border-2 ${isHovered ? 'bg-purple-500' : 'bg-white'}`}
-                  >
-                    <MoveUpRight className={`w-4 h-4 ${isHovered ? 'text-white' : 'text-gray-700'}`} />
+                  <div className={`w-7 h-7 rounded-full flex items-center justify-center hover:bg-purple-500 border-white border-2 ${isHovered ? 'bg-purple-500' : 'bg-white'}`}>
+                    <MoveUpRight className={`w-4 h-4 ${isHovered ? 'hidden' : 'text-gray-700'}`} />
+                    <MoveUpRight className={`w-4 h-4 ${isHovered ? 'text-white ' : 'hidden'}`} />
                   </div>
                   <span className="ml-3">
                     Register Now
                   </span>
                 </div>
-              </button>
+              </Button>
             </div>
             <div className="bg-primary-radial-gradient sm:w-[130px] w-[80px] h-[80px] sm:h-[130px] rounded-full absolute top-[25%] left-[15%] lg:top-[25%] lg:left-[15%]"></div>
             <div className="bg-primary-radial-gradient sm:w-[130px] w-[80px] h-[80px] sm:h-[130px] rounded-full absolute top-[20%] left-[65%] lg:top-[15%] lg:left-[75%]"></div>
