d9e3aaa9135bb56e2d5ba4a85facf5857d62d8e0
kgaurav152 <kgaurav152@gmail.com>
2023-12-26 03:19:54 +0530

    fixed deployment issue

--- app/(dashboard)/(routes)/admin/participant/pending/buttonBar.jsx
+++ app/(dashboard)/(routes)/admin/participant/pending/buttonBar.jsx
@@ -15,6 +15,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Trash } from "lucide-react";
 
 
 export function AcceptApprovalForm({ setOpen,  userId, setActionSuccess }) {
