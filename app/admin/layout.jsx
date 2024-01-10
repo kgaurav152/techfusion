fe4a12adfbb489adabd7e607acacc62894d794fe
thePiyushPriyadarshi <piyushjha.ktr@gmail.com>
2024-01-10 13:10:31 +0530

    Admin route protected

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -1,12 +1,23 @@
 "use client";
 import AdminNavBar from "@/components/adminNavbar";
 import Footer from '@/components/footer';
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 // import { useEffect } from "react";
 // import Network from "@/components/network";
 
 const DashboardLayout = ({ children }) => {
 
+  const {user} = useSelector((state)=>state.profile);
+
+  useEffect(()=>{
+
+    if(user?.userType !=="admin"){
+      redirect("/");
+    }
+  },[user]);
   return (      
     <div>
       <div className=" min-h-[100vh] bg-[#00040F] relative" >
