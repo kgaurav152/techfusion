2154f98e13eddb5aab295ba0bf20395f3c06ddeb
kgaurav152 <kgaurav152@gmail.com>
2024-01-17 05:21:13 +0530

    updated website dev details

--- public/coordinators.jsx
+++ public/coordinators.jsx
@@ -8,16 +8,26 @@ export const eventCoordinators = [
         email: "kgaurav152@gmail.com",
         linkedin:"https://www.linkedin.com/in/kgaurav152",
         instaId:"https://www.instagram.com/kgaurav152",
+        github:"https://github.com/kgaurav152",
         pictureUrl:"https://i.imgur.com/vZkbfL0.png",
-        mobile: "7004174269"
+        mobile: "7004174269",
+        knowMoreUrl:"https://linktr.ee/kgaurav152",
+        label:"Frontend UI/UX",
+        batch:"2K21",
+        branch:"C.S.E"
         },
         {
             name: "Piyush Priyadarshi",
             email: "piyushjha.code@gmail.com",
             linkedin:"https://www.linkedin.com/in/thepiyushpriyadarshi",
             instaId:"https://www.instagram.com/piyush._.priyadarshi",
+            github:"https://github.com/thePiyushPriyadarshi",
             pictureUrl:"https://i.imgur.com/IymjlZl.jpg",
-            mobile: "9572165854"
+            mobile: "9572165854",
+            knowMoreUrl:"https://www.instagram.com/piyush._.priyadarshi",
+            label:"Backend",
+            batch:"2K20",
+            branch:"C.S.E"
         }   
       ]
     },
