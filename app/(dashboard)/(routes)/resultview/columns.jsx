72083f889f117a03c7edd63403b7ff1a71c8939c
thePiyushPriyadarshi <piyushjha.ktr@gmail.com>
2024-01-27 16:11:28 +0530

    team member details added

--- app/(dashboard)/(routes)/resultview/columns.jsx
+++ app/(dashboard)/(routes)/resultview/columns.jsx
@@ -1,57 +1,69 @@
-"use client"
+"use client";
 
-import Link from 'next/link';
+import Link from "next/link";
 import { ColumnDef } from "@tanstack/react-table";
 import { ArrowUpDown } from "lucide-react";
-import { Button } from "@/components/ui/button"; 
-  
+import { Button } from "@/components/ui/button";
+
 // import { ApproveButton, RejectButton } from "@/app/admin/participant/all/buttonBar";
 
-export const columns = (setParticipantData,allResultId) => [
+export const columns = (setParticipantData, allResultId) => [
   {
-    accessorKey: 'festId',
-    header: 'TechFusion ID',
+    accessorKey: "festId",
+    header: "TechFusion ID",
     cell: ({ row }) => {
-        const  {participant}= row.original
-        
-        return (
-          <div>
-            <p>
-            <span>{participant.participants[0].festId}</span></p> 
-          </div>
-        )
-      },
-  }, 
-  {
-    accessorKey: 'Team Leader Name',
-    header: "Team  Leader Name",
-    cell: ({ row }) => {
-        const  {participant}= row.original
-        
-        return (
-          <div>
-            <p>
-            <span>{participant.participants[0].name}</span></p> 
-          </div>
-        )
-      },
+      const { participant } = row.original;
+
+      return (
+        <div>
+          <p>
+            <span>{participant.participants[0].festId}</span>
+          </p>
+        </div>
+      );
+    },
   },
   {
-    accessorKey: 'Team Name',
+    accessorKey: "Team Name",
     header: "Team Name",
     cell: ({ row }) => {
-        const  {participant}= row.original
-        
-        return (
-          <div>
-            <p>
-            <span>{participant.teamName}</span></p> 
-          </div>
-        )
-      },
+      const { participant } = row.original;
+
+      return (
+        <div>
+          <p>
+            <span>{participant.teamName}</span>
+          </p>
+        </div>
+      );
+    },
   },
   {
-    accessorKey: 'college',
+    accessorKey: "Team Leader Name",
+    header: "Team  Member Name",
+    cell: ({ row }) => {
+      const { participant } = row.original;
+
+      return (
+        <div>
+          <p>
+            <span>{participant.participants[0]?.name}</span>
+          </p>
+          <p>
+            <span>{participant.participants[1]?.name}</span>
+          </p>
+          <p>
+            <span>{participant.participants[2]?.name}</span>
+          </p>
+          <p>
+            <span>{participant.participants[3]?.name}</span>
+          </p>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "college",
     header: ({ column }) => {
       return (
         <Button
@@ -61,21 +73,22 @@ export const columns = (setParticipantData,allResultId) => [
           College
           <ArrowUpDown className="ml-2 h-4 w-4" />
         </Button>
-      )
+      );
     },
     cell: ({ row }) => {
-        const  {participant}= row.original
-        
-        return (
-          <div>
-            <p>
-            <span>{participant.participants[0].college}</span></p> 
-          </div>
-        )
-      },
+      const { participant } = row.original;
+
+      return (
+        <div>
+          <p>
+            <span>{participant.participants[0].college}</span>
+          </p>
+        </div>
+      );
+    },
   },
   {
-    accessorKey: 'Rank',
+    accessorKey: "Rank",
     header: ({ column }) => {
       return (
         <Button
@@ -85,47 +98,48 @@ export const columns = (setParticipantData,allResultId) => [
           Rank
           <ArrowUpDown className="ml-2 h-4 w-4" />
         </Button>
-      )
+      );
     },
     cell: ({ row }) => {
-      const  {rank}= row.original
-      
+      const { rank } = row.original;
+
       return (
         <div>
           <p>
-          <span> {rank}</span></p> 
+            <span> {rank}</span>
+          </p>
         </div>
-      )
+      );
     },
   },
   {
-    accessorKey: 'Score',
-    header:  "Score",
+    accessorKey: "Score",
+    header: "Score",
     cell: ({ row }) => {
-      const  {score}= row.original
-      
+      const { score } = row.original;
+
       return (
         <div>
           <p>
-          <span> {score}</span></p> 
+            <span> {score}</span>
+          </p>
         </div>
-      )
+      );
     },
   },
-{
-    accessorKey: 'Description',
-    header:  "Description",
+  {
+    accessorKey: "Description",
+    header: "Description",
     cell: ({ row }) => {
-      const  {description}= row.original
-      
+      const { description } = row.original;
+
       return (
         <div>
           <p>
-          <span> {description}</span></p> 
+            <span> {description}</span>
+          </p>
         </div>
-      )
+      );
     },
-},
-]
-
-  
+  },
+];
