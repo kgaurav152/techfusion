c3e8fc9b1ad7db2883a6c6363e5b875f0434d46d
kgaurav152 <kgaurav152@gmail.com>
2024-11-10 19:17:52 +0530

    modified mailService to make sendEmail calls in await to avoid premature termination

--- helpers/mailService.js
+++ helpers/mailService.js
@@ -31,7 +31,7 @@ export const sendResetPasswordEmail = async (email, resetUrl) => {
                <p><a href="${resetUrl}">${resetUrl}</a></p><br/>
                <p><b>Ignore this mail if you haven't request to reset your password.</b></p>`,
   };
-  return sendEmail(mailOptions);
+  return await sendEmail(mailOptions);
 };
 
 // Method to send onboarding email
@@ -42,7 +42,7 @@ export const sendOnboardingEmail = async (email, username) => {
     subject: "Welcome to TechFusion!",
     html: `<p>Hi ${username}, <br/>welcome to TechFusion online platform!<br/><a href="https://techfusion.org.in">https://techfusion.org.in</a></p><br /><p>You can use our platform to manage your event participation in TechFusion online!</p><br/><br/><br/><p>Sorry, for the inconveniance but for now your account will remain in pending state, you can login and explore the platform till your account is verified!</p><br/><br/><p>Your account will be verified upon payment confirmation, usually it takes 24-72 hours.</p><br/><br/> <p>Thanks and regards,<br/>Web Team<br/>TechFusion<br/>KEC,Katihar</p>`,
   };
-  return sendEmail(mailOptions);
+  return await sendEmail(mailOptions);
 };
 
 // Method to send verification confirmation email
@@ -57,7 +57,7 @@ export const sendVerificationSuccessfulEmail = async (
     subject: "TechFusion account verified!",
     html: `<p>Hi ${username}, <br/>welcome to TechFusion online platform!<br/><a href="https://techfusion.org.in">https://techfusion.org.in</a></p><br /><p>You can use our platform to manage your event participation in TechFusion online!</p><br/><br/><br/><p>Your account have been verified successfully, you can <a href="https://techfusion.org.in/sign-in">login</a> and explore the platform as well as participate in the events.<br/><br/>Registered Email: ${email}<br/>TechFusion Id: ${festId}</p><br/><br/> <p>Thanks and regards,<br/>Web Team<br/>TechFusion<br/>KEC,Katihar</p>`,
   };
-  return sendEmail(mailOptions);
+  return await sendEmail(mailOptions);
 };
 
 // Method to send verification rejection email
@@ -68,5 +68,5 @@ export const sendVerificationUnSuccessfulEmail = async (email, username) => {
     subject: "TechFusion account verification failed!",
     html: `<p>Hi ${username}, <br/>Your payment verification for participating in TechFusion have failed, and hence your account have been deleted from our platform.<br/> For any help contact us at <a href="https://techfusion.org.in/contact-us">https://techfusion.org.in/contact-us</a><br/><br/><br/>Think this is a mistake, Possible reason for rejection:<br/><ol><li>Payment Screenshot uploaded is either blurry or wrong.</li><li>The transection id of payment isn't clearly visible in the payment screenshot.</li></ol><br/><br/>You can always re-register by signing up with correct details at <a href="https://techfusion.org.in//registration">https://techfusion.org.in//registration</a></p><br/> <p>Thanks and regards,<br/>Web Team<br/>TechFusion<br/>KEC,Katihar</p>`,
   };
-  return sendEmail(mailOptions);
+  return await sendEmail(mailOptions);
 };
