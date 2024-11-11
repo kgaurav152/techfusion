3a655e8eedc708f8c8f54eb4b57343c5d68b9663
thePiyushPriyadarshi <piyushjha.ktr@gmail.com>
2024-11-12 01:28:48 +0530

    feat: delete participation by admin api

---
+++ app/api/admin/deletePaticipation/route.js
@@ -0,0 +1,78 @@
+import { connect } from "@/config/dbconfig";
+import User from "@/models/User";
+// import bcryptjs from "bcryptjs";
+import { NextResponse } from "next/server";
+// import jwt from "jsonwebtoken"
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+import Participation from "@/models/Participation";
+
+connect();
+export async function POST(req) {
+  const { token, participation_id } = await req.json();
+  try {
+    const userID = await getDataFromToken(token);
+    const user = await User.findById(userID);
+
+    if (user?.userType !== "admin") {
+        return NextResponse.json({
+          success: false,
+          message: "This is protected route for Admin access",
+        });
+      }
+    const participation = await Participation.findById(
+      participation_id
+    ).populate("event");
+
+
+    if (participation.event.eventType === "Technical") {
+      for (let i = 0; i < participation.participants.length; i++) {
+        await User.findByIdAndUpdate(participation.participants[i], {
+          $pull: { technical: participation._id },
+        });
+      }
+    } else if (participation.event.eventType === "Cultural") {
+      for (let i = 0; i < participation.participants.length; i++) {
+        await User.findByIdAndUpdate(participation.participants[i], {
+          $pull: { cultural: participation._id },
+        });
+      }
+    }
+    await Participation.findByIdAndDelete(participation._id);
+    const data = await User.findById(user._id)
+      .populate({
+        path: "technical",
+        populate: {
+          path: "event",
+        },
+      })
+      .populate({
+        path: "technical",
+        populate: {
+          path: "participants",
+        },
+      })
+      .populate({
+        path: "cultural",
+        populate: {
+          path: "event",
+        },
+      })
+      .populate({
+        path: "cultural",
+        populate: {
+          path: "participants",
+        },
+      });
+    return NextResponse.json({
+      success: true,
+      message: "Participation to Event was deleted successfully",
+      data: data,
+    });
+  } catch (err) {
+    return NextResponse.json({
+      error: err.message,
+      success: false,
+      message: "Unable to Delete Event Participation",
+    });
+  }
+}
