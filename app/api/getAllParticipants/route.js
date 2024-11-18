3a8dcda1f70e60ad5fbf6f3cd6ac8502574f4023
kgaurav152 <kgaurav152@gmail.com>
2024-11-18 12:14:00 +0530

    modified getAllParticipants api to return only participant type user rather than other also

--- app/api/getAllParticipants/route.js
+++ app/api/getAllParticipants/route.js
@@ -6,30 +6,31 @@ import { NextResponse } from "next/server";
 import { getDataFromToken } from "@/helpers/getDataFromToken";
 
 connect();
-export async function POST(req){
-    const {token} = await req.json()
-    try{
-        const userID = await getDataFromToken(token);
-        const user = await User.findById(userID);
-        if(user?.userType!=="admin"){
-            return NextResponse.json({
-                success: false,
-                message:"This is protected route for Admin access"
-            })
-        }
-        const data = await User.find({status:{ $eq: "approved" },userType:{$ne: "admin"}}); 
-        return NextResponse.json({
-            success: true,
-            message:"All Participant with Approved Status",
-            data:data,
-        })
-
-    }catch(err){
-        return NextResponse.json({
-            error: err.message,
-            success:false,
-            message: "Unable to fetch All Participants"
-        })
+export async function POST(req) {
+  const { token } = await req.json();
+  try {
+    const userID = await getDataFromToken(token);
+    const user = await User.findById(userID);
+    if (user?.userType !== "admin") {
+      return NextResponse.json({
+        success: false,
+        message: "This is protected route for Admin access",
+      });
     }
-
+    const data = await User.find({
+      status: { $eq: "approved" },
+      userType: { $eq: "participant" },
+    });
+    return NextResponse.json({
+      success: true,
+      message: "All Participant with Approved Status",
+      data: data,
+    });
+  } catch (err) {
+    return NextResponse.json({
+      error: err.message,
+      success: false,
+      message: "Unable to fetch All Participants",
+    });
+  }
 }
