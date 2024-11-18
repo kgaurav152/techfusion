03d88ac3f6c94c60163495b93dff2e589655911b
kgaurav152 <kgaurav152@gmail.com>
2024-11-18 11:53:23 +0530

    updated dbconfig for newly added schemas/models

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -7,41 +7,48 @@
 //   if(mongoose.connections[0].readyState) return;
 //     try{
 //         await mongoose.connect(process.env.MONGO_URL);
-//         mongoose.model('User',User) 
-//         mongoose.model('Event',Event) 
-//         mongoose.model('Participation',Participation) 
+//         mongoose.model('User',User)
+//         mongoose.model('Event',Event)
+//         mongoose.model('Participation',Participation)
 //         console.log("Connected to MongoDB");
 //     }
 //     catch(error){
 //         throw new Error("Error in connecting to MongoDB",error)
 //     }
-   
+
 // }
 
 import Event from "@/models/Event";
 import Participation from "@/models/Participation";
 import User from "@/models/User";
+import CampusAmbassador from "@/models/CampusAmbassador";
+import Coordinator from "@/models/Coordinator";
+import ResetPasswordToken from "@/models/ResetPasswordToken";
+import Result from "@/models/Result";
+import ResultDetails from "@/models/ResultDetails";
+
 import mongoose from "mongoose";
 
 export async function connect() {
   try {
     await mongoose.connect(process.env.MONGO_URL);
     const connnection = mongoose.connection;
-   
-    connnection.on('connected', ()=>{
-      // mongoose.model('User',User) 
-      // mongoose.model('Participation',Participation) 
-      // mongoose.model('Event',Event) 
-      console.log('MongoDB connected successfully');
-    })
 
-    connnection.on('error', (err)=>{
-      console.log('MongoDB connection error. Please make sure MongoDB is running.' + err);
+    connnection.on("connected", () => {
+      // mongoose.model('User',User)
+      // mongoose.model('Participation',Participation)
+      // mongoose.model('Event',Event)
+      console.log("MongoDB connected successfully");
+    });
+
+    connnection.on("error", (err) => {
+      console.log(
+        "MongoDB connection error. Please make sure MongoDB is running." + err
+      );
       process.exit();
-    })
-
+    });
   } catch (error) {
-    console.log('Something gone wrong!');
+    console.log("Something gone wrong!");
     console.log(error);
   }
 }
