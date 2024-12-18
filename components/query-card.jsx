5b10f2ed6f63dde81816d4b64c8bc5936880e319
kgaurav152 <kgaurav152@gmail.com>
2024-12-19 04:09:38 +0530

    changed contact person order in query card

--- components/query-card.jsx
+++ components/query-card.jsx
@@ -1,5 +1,11 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import React from "react";
 
 const QueryCard = () => {
@@ -10,25 +16,6 @@ const QueryCard = () => {
         <CardDescription />
       </CardHeader>
       <CardContent className="grid gap-4 lg:gap-2 lg:grid-cols-2">
-        <div className="flex items-center justify-between space-x-4">
-          <div className="flex items-center space-x-4">
-            <Avatar>
-              <AvatarImage src="avatar_02.png" />
-              <AvatarFallback>MK</AvatarFallback>
-            </Avatar>
-            <div className="gap-1">
-              <p className="text-sm font-medium leading-none">Mohit Kumar</p>
-              <a
-                href="https://wa.me/917257827104?text=Hello!%20I%20have%20some%20Query%20related%20to%20Event%20Registration."
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-sm text-blue-800"
-              >
-                +917257827104
-              </a>
-            </div>
-          </div>
-        </div>
         <div className="flex items-center justify-between space-x-4">
           <div className="flex items-center space-x-4">
             <Avatar>
@@ -44,6 +31,25 @@ const QueryCard = () => {
                 className="text-sm text-blue-800"
               >
                 +917004174269
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center justify-between space-x-4">
+          <div className="flex items-center space-x-4">
+            <Avatar>
+              <AvatarImage src="avatar_02.png" />
+              <AvatarFallback>MK</AvatarFallback>
+            </Avatar>
+            <div className="gap-1">
+              <p className="text-sm font-medium leading-none">Mohit Kumar</p>
+              <a
+                href="https://wa.me/917257827104?text=Hello!%20I%20have%20some%20Query%20related%20to%20Event%20Registration."
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-sm text-blue-800"
+              >
+                +917257827104
               </a>
             </div>
           </div>
